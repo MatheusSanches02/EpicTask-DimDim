d6cdb2ab6f4304e69d97cdf09ad123c780156844
MatheusSanches02 <rm87426@fiap.com.br>
2022-10-01 03:26:46 -0300

    arquivo de ddl banco de dados

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/web/TaskWebController.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/web/TaskWebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,21 +22,22 @@ public class TaskWebController {
     TaskService service;
 
     @GetMapping
-    public ModelAndView index(){
+    public ModelAndView index() {
         return new ModelAndView("task/index")
                 .addObject("tasks", service.listAll());
     }
 
     @GetMapping("new")
-    public String form(){
+    public String form() {
         return "task/form";
     }
 
     @PostMapping
-    public String create(@Valid Task task, BindingResult result){
-        if( result.hasErrors() ) return "task/form";
+    public String create(@Valid Task task, BindingResult result) {
+        if (result.hasErrors())
+            return "task/form";
         service.save(task);
         return "redirect:/task";
     }
-    
+
 }
